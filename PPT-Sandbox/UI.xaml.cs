a29340d1e09a8d3860c3794bb21ff54891d30a8d
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-11-14 16:14:34 +0100

    Clean up namespace references, IntPtr creation, better byte[] conversion

--- PPT-Sandbox/UI.xaml.cs
+++ PPT-Sandbox/UI.xaml.cs
@@ -47,138 +47,120 @@ namespace Sandbox {
                     break;
             }
         }
+
+        private byte[] ConvertByteString(string bytes) =>
+            bytes.Split(' ').Select(i => Convert.ToByte(i, 16)).ToArray();
+
         private void Pento(object sender, RoutedEventArgs e) {
-            int addr = Game.ReadInt32(new IntPtr(0x140463F20));
+            IntPtr addr = (IntPtr)Game.ReadUInt64(new IntPtr(0x140463F20));
 
             if (PentominoVersus.IsChecked == true) {
                 Game.WriteByte(
-                    new System.IntPtr(addr), 5          //S
+                    addr, 5          //S
                 );
 
                 Game.WriteInt32(
-                    new System.IntPtr(addr + 0x24), -2
+                    addr + 0x24, -2
                 );
 
-
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0xE0), 5   //Z
+                    addr + 0xE0, 5   //Z
                 );
 
                 Game.WriteByteArray(
-                    new System.IntPtr(addr + 0x104),
-                    (from i in "01 00 00 00 FF FF FF FF".Split(new char[]
-                        {
-                            ' '
-                        })
-                     select Convert.ToByte(i, 16)).ToArray<byte>()
+                    addr + 0x104,
+                    ConvertByteString("01 00 00 00 FF FF FF FF")
                 );
 
-
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x1C0), 5          //J
+                    addr + 0x1C0, 5          //J
                 );
 
                 Game.WriteInt32(
-                    new System.IntPtr(addr + 0x1E8), -1
+                    addr + 0x1E8, -1
                 );
 
-
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x2A0), 5          //L
+                    addr + 0x2A0, 5          //L
                 );
 
                 Game.WriteByteArray(
-                    new System.IntPtr(addr + 0x2C4),
-                    (from i in "FF FF FF FF FF FF FF FF".Split(new char[]
-                        {
-                            ' '
-                        })
-                     select Convert.ToByte(i, 16)).ToArray<byte>()
+                    addr + 0x2C4,
+                    ConvertByteString("FF FF FF FF FF FF FF FF")
                 );
 
-
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x380), 5          //T
+                    addr + 0x380, 5          //T
                 );
 
                 Game.WriteInt32(
-                    new System.IntPtr(addr + 0x3A8), 2
+                    addr + 0x3A8, 2
                 );
 
-
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x460), 5          //O
+                    addr + 0x460, 5          //O
                 );
 
                 Game.WriteInt32(
-                    new System.IntPtr(addr + 0x484), -1
+                    addr + 0x484, -1
                 );
 
-
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x540), 5          //I
+                    addr + 0x540, 5          //I
                 );
 
                 Game.WriteInt32(
-                    new System.IntPtr(addr + 0x564), -2
+                    addr + 0x564, -2
                 );
+
             } else {
                 Game.WriteByte(
-                    new System.IntPtr(addr), 4                  //S
+                    addr, 4                  //S
                 );
 
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0xE0), 4           //Z
+                    addr + 0xE0, 4           //Z
                 );
 
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x1C0), 4          //J
+                    addr + 0x1C0, 4          //J
                 );
 
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x2A0), 4          //L
+                    addr + 0x2A0, 4          //L
                 );
 
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x380), 4          //T
+                    addr + 0x380, 4          //T
                 );
 
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x460), 4          //O
+                    addr + 0x460, 4          //O
                 );
 
                 Game.WriteByte(
-                    new System.IntPtr(addr + 0x540), 4          //I
+                    addr + 0x540, 4          //I
                 );
             }
         }
 
         private void LineDelay(object sender, RoutedEventArgs e) {
-            if (RemoveLineClearDelay.IsChecked == true) {
-                Game.WriteByte(
-                    new System.IntPtr(0x1400A26F4), 0
-                );
-            } else {
-                Game.WriteByte(
-                    new System.IntPtr(0x1400A26F4), 1
-                );
-            }
+            Game.WriteByte(
+                new IntPtr(0x1400A26F4),
+                Convert.ToByte(RemoveLineClearDelay.IsChecked != true)
+            );
         }
 
         private void Hold(object sender, RoutedEventArgs e) {
-            byte[] write;
-
-            if (UndoHold.IsChecked == true) {
-                write = new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
-            } else {
-                write = new byte[] { 0xFF, 0x83, 0x48, 0x1, 0x0, 0x0 };
-            }
+            byte[] write = (UndoHold.IsChecked == true)
+                ? new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 }
+                : new byte[] { 0xFF, 0x83, 0x48, 0x01, 0x00, 0x00 };
 
             Game.WriteByteArray(
-                new System.IntPtr(0x142852508), write
+                new IntPtr(0x142852508), write
             );
             Game.WriteByteArray(
-                new System.IntPtr(0x14285252F), write
+                new IntPtr(0x14285252F), write
             );
         }
 
@@ -186,40 +168,24 @@ namespace Sandbox {
             if (SecretGradeGarbage.IsChecked == true) {
 
                 Game.WriteByteArray(
-                    new System.IntPtr(0x14009F480), 
-                    (from i in "8A 1D 7A 3B 3C 00 80 3D 74 3B 3C 00 00 75 08 FF 05 6B 3B 3C 00 EB 06 FF 0D 63 3B 3C 00 80 3D 5D 3B 3C 00 00 75 10 80 3D 53 3B 3C 00 09 7C 27 C6 05 4B 3B 3C 00 01 80 3D 43 3B 3C 00 00 75 17 C6 05 3B 3B 3C 00 00 80 3D 33 3B 3C 00 0A 72 07 C6 05 2A 3B 3C 00 00 E9 85 3B 61 02".Split(new char[]
-                        {
-                            ' '
-                        })
-                    select Convert.ToByte(i, 16)).ToArray<byte>()
+                    new IntPtr(0x14009F480),
+                    ConvertByteString("8A 1D 7A 3B 3C 00 80 3D 74 3B 3C 00 00 75 08 FF 05 6B 3B 3C 00 EB 06 FF 0D 63 3B 3C 00 80 3D 5D 3B 3C 00 00 75 10 80 3D 53 3B 3C 00 09 7C 27 C6 05 4B 3B 3C 00 01 80 3D 43 3B 3C 00 00 75 17 C6 05 3B 3B 3C 00 00 80 3D 33 3B 3C 00 0A 72 07 C6 05 2A 3B 3C 00 00 E9 85 3B 61 02")
                 );
 
                 Game.WriteByteArray(
-                    new System.IntPtr(0x140061010),
-                    (from i in "66 C7 05 E7 1F 40 00 00 00 E9 A2 79 91 01".Split(new char[]
-                        {
-                            ' '
-                        })
-                    select Convert.ToByte(i, 16)).ToArray<byte>()
+                    new IntPtr(0x140061010),
+                    ConvertByteString("66 C7 05 E7 1F 40 00 00 00 E9 A2 79 91 01")
                 );
 
             } else {
                 Game.WriteByteArray(
-                    new System.IntPtr(0x14009F480),
-                    (from i in "E9 DB 3B 61 02".Split(new char[]
-                       {
-                            ' '
-                       })
-                     select Convert.ToByte(i, 16)).ToArray<byte>()
+                    new IntPtr(0x14009F480),
+                    ConvertByteString("E9 DB 3B 61 02")
                 );
 
                 Game.WriteByteArray(
-                    new System.IntPtr(0x140061010),
-                    (from i in "E9 AB 79 91 01".Split(new char[]
-                       {
-                            ' '
-                       })
-                     select Convert.ToByte(i, 16)).ToArray<byte>()
+                    new IntPtr(0x140061010),
+                    ConvertByteString("E9 AB 79 91 01")
                 );
             }
         }
